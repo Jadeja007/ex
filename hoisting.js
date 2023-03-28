8431c73b82e35922f6c6eea8fd4c8595a91a9daf
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-03-28 13:07:52 +0530

    Updated file

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+
+var x = 2;
+console.log(x)
+
+const quote = "He read \"The Cremation of Sam McGee by\" R.W. Service.";
+console.log(quote);
+
+const file = "c:\\temp";
+console.log(file)
+
+const str = "this line \
+are broken \
+top to bottom."
+console.log(str)
